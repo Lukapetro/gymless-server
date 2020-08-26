c56ebd49918240ea44e33ef143a24c9dd9fb4f9a
Luka Petrovic <lukapetro@yahoo.it>
2020-08-26 11:47:40 +0200

    payment Intent

--- src/graphql/mutations/PaymentIntet.js
+++ src/graphql/mutations/PaymentIntet.js
@@ -1,13 +1,50 @@
-import { mutationField } from '@nexus/schema'
+import { mutationField, intArg, stringArg } from '@nexus/schema'
 import { stripe } from '../../stripe'
 
 export const paymentIntent = mutationField('paymentIntent', {
-  type: 'String',
-  resolve: async (parent, { id }, ctx) => {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: 1000,
-      currency: 'eur',
+  type: 'User',
+  args: {
+    id: intArg({ required: true }),
+    amount: intArg({ required: true }),
+    paymentMethod: stringArg({ required: true }),
+  },
+  resolve: async (parent, { id, amount, paymentMethod }, ctx) => {
+    const user = await ctx.prisma.user.findOne({
+      where: {
+        id: ctx.userId,
+      },
     })
+
+    if (!user) {
+      throw new Error(`Non autorizzato`)
+    }
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'eur',
+      customer: user.customerId,
+      payment_method: paymentMethod,
+      confirm: true,
+    })
+
+    console.log('intent: ', paymentIntent)
+
+    if (paymentIntent.status === 'succeeded') {
+      return ctx.prisma.workout.update({
+        data: {
+          partecipants: {
+            connect: [
+              {
+                id: ctx.userId,
+              },
+            ],
+          },
+        },
+        where: {
+          id: Number(id),
+        },
+      })
+    }
 
     return paymentIntent.client_secret
   },
